0519f3fedcc11119891ff535e1a3519bf3aa3369
Stefan Vigerske <svigerske@gams.com>
2017-05-19 17:48:37 +0200

    check for SolveEngine API key in environment

--- gamsse.c
+++ gamsse.c
@@ -13,6 +13,7 @@ int main(int argc, char** argv)
    int rc = EXIT_FAILURE;
    char buffer[1024];
    char lpfilename[300];
+   char* apikey;
    int status;
    double objMinMaxFac;
 
@@ -22,57 +23,63 @@ int main(int argc, char** argv)
       return 1;
    }
 
-   /*
-   * GAMS initialize GMO and GEV libraries
-   */
+   /* GAMS initialize GMO and GEV libraries */
    if (!gmoCreate(&gmo, buffer, sizeof(buffer)) || !gevCreate(&gev, buffer, sizeof(buffer)))
    {
       fprintf(stderr, "%s\n", buffer);
       goto TERMINATE;
    }
 
-   /*
-   * GAMS load control file
-   */
+   /* GAMS load control file */
    if (gevInitEnvironmentLegacy(gev, argv[1]))
    {
       fprintf(stderr, "Could not load control file %s\n", argv[1]);
       goto TERMINATE;
    }
 
-   /*
-   * GAMS let gmo know about gev
-   */
+   /* GAMS let gmo know about gev */
    if (gmoRegisterEnvironment(gmo, gev, buffer))
    {
       fprintf(stderr, "Error registering GAMS Environment: %s\n", buffer);
       goto TERMINATE;
    }
 
-   /*
-   * GAMS load instance data
-   */
+   /* GAMS load instance data */
    if (gmoLoadDataLegacy(gmo, buffer))
    {
       fprintf(stderr, "Could not load model data.\n");
       goto TERMINATE;
    }
 
-   gevLogStat(gev, "This is GAMS/SolveEngine.");
+   gevLogStat(gev, "This is the GAMS link to Satalia SolveEngine.");
 
-   sprintf(lpfilename, "%sproblem.lp", gevGetStrOpt(gev, gevNameScrDir, buffer));
-   printf("Writing LP to %s\n", lpfilename);
-   writeLP(gmo, gev, lpfilename);
+   /* check whether we have an API key for SolveEngine */
+   apikey = getenv("SOLVEENGINE_APIKEY");
+   if( apikey == NULL )
+   {
+      gevLogStat(gev, "No SolveEngine API key found in environment (SOLVEENGINE_APIKEY). Exiting.");
+      goto TERMINATE;
+   }
+   else
+   {
+      gevLogPChar(gev, "API key: ");
+      gevLog(gev, apikey);
+   }
 
-
-   /*
-   * GAMS general
-   */
+   /* get the problem into a normal form */
    gmoObjStyleSet(gmo, gmoObjType_Fun);
    gmoObjReformSet(gmo, 1);
    gmoIndexBaseSet(gmo, 0);
    gmoSetNRowPerm(gmo); /* hide =N= rows */
    objMinMaxFac = (gmoSense(gmo) == gmoObj_Max) ? -1.0 : 1.0;
+
+   /* make LP file from problem */
+   sprintf(lpfilename, "%sproblem.lp", gevGetStrOpt(gev, gevNameScrDir, buffer));
+   printf("Writing LP to %s\n", lpfilename);
+   writeLP(gmo, gev, lpfilename);
+
+
+   /* remove(lpfilename); */
 
    gmoUnloadSolutionLegacy(gmo);
 
